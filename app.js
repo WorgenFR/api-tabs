50400dd5d703fc9da25fcd64bf7c1bf0dcb009eb
GRAPPIN Antoine <agrappin@insitaction.com>
2024-03-26 11:09:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('MyDB.db')
+const app = express();
+
+//GET all songs
+app.use('/api/songs',(req, res) => {
+     db.all("SELECT s.ID, s.TITLE, a.TITLE AS 'artist', t.LIBELLE AS 'tuning' FROM songs s INNER JOIN artists a ON s.ARTIST_ID = A.ID INNER JOIN tunings t ON s.TUNING_ID = t.ID", function (err, rows) {
+        console.log(rows)
+        res.json(rows)
+    })
+});
+
+//GET one song (id)
+app.use('/api/song_by_id/:id',(req, res) => {
+    let id = req.params.id;
+    db.all(`SELECT s.ID, s.TITLE, a.TITLE AS 'artist', t.LIBELLE AS 'tuning' FROM songs s INNER JOIN artists a ON s.ARTIST_ID = A.ID INNER JOIN tunings t ON s.TUNING_ID = t.ID WHERE s.ID = ${id}`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
+//SEARCH song by title
+app.use('/api/song_by_title/:title',(req, res) => {
+    let title = req.params.title;
+    db.all(`SELECT s.ID, s.TITLE, a.TITLE AS 'artist', t.LIBELLE AS 'tuning' FROM songs s INNER JOIN artists a ON s.ARTIST_ID = A.ID INNER JOIN tunings t ON s.TUNING_ID = t.ID WHERE s.TITLE LIKE '%${title}%'`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
+//POST new song
+app.use('/api/add_song/:title/:artist_id/:tuning_id',(req, res) => {
+    let title = req.params.title;
+    let artist_id = req.params.artist_id;
+    let tuning_id = req.params.tuning_id;
+    db.run(`INSERT INTO songs (TITLE, ARTIST_ID, TUNING_ID) VALUES (?, ?, ?)`, [title, artist_id, tuning_id], function (err) {
+        console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//UPDATE one song 
+app.use('/api/update_song/:id/:title/:artist_id/:tuning_id',(req, res) => {
+    let id = req.params.id;
+    let title = req.params.title;
+    let artist_id = req.params.artist_id;
+    let tuning_id = req.params.tuning_id;
+    db.run(`UPDATE songs SET title = ?, artist_id = ?, tuning_id = ? WHERE id = ?`, [title, artist_id, tuning_id, id], function (err) {
+        console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//DELETE a song (id)
+app.use('/api/delete_song/:id',(req, res) => {
+    let id = req.params.id;
+    db.run(`DELETE FROM songs WHERE ID = ${id}`, function (err) {
+        console.log(id)
+       res.json({message: 'success'})
+   })
+});
+
+//GET all artists
+app.use('/api/artists',(req, res) => {
+    db.all('SELECT ID, TITLE FROM artists', function (err, rows) {
+        console.log(rows);
+       res.json(rows)
+   })
+});
+
+//GET one artist (id)
+app.use('/api/artist_by_id/:id',(req, res) => {
+    let id = req.params.id;
+    db.all(`SELECT ID, TITLE FROM artists WHERE ID = ${id}`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
+//SEARCH artist by title
+app.use('/api/artist_by_title/:title',(req, res) => {
+    let title = req.params.title;
+    db.all(`SELECT ID, TITLE FROM artists WHERE TITLE LIKE '%${title}%'`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
+//POST new artist
+app.use('/api/add_artist/:title',(req, res) => {
+    let title = req.params.title;
+    db.run(`INSERT INTO artists (TITLE) VALUES (?)`, [title], function (err) {
+        console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//UPDATE one artist 
+app.use('/api/update_artist/:id/:title',(req, res) => {
+    let id = req.params.id;
+    let title = req.params.title;
+    db.run(`UPDATE artists SET title = ? WHERE id = ?`, [title, id], function (err) {
+        console.log(title)
+       res.json({message: 'success'})
+   })
+});
+
+//DELETE an artist (id)
+app.use('/api/delete_artist/:id',(req, res) => {
+    let id = req.params.id;
+    db.run(`DELETE FROM artists WHERE ID = ${id}`, function (err) {
+        console.log(id)
+       res.json({message: 'success'})
+   })
+});
+
+//GET all tunings
+app.use('/api/tunings',(req, res) => {
+    db.all('SELECT ID, LIBELLE FROM tunings', function (err, rows) {
+        console.log(rows);
+       res.json(rows)
+   })
+});
+
+//GET one tuning (id)
+app.use('/api/tuning_by_id/:id',(req, res) => {
+    let id = req.params.id;
+    db.all(`SELECT ID, LIBELLE FROM tunings WHERE ID = ${id}`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
+//SEARCH tuning by libelle
+app.use('/api/tuning_by_libelle/:libelle',(req, res) => {
+    let libelle = req.params.libelle;
+    db.all(`SELECT ID, LIBELLE FROM tunings WHERE LIBELLE LIKE '%${libelle}%'`, function (err, rows) {
+       res.json(rows)
+   })
+});
+
+//POST new tuning
+app.use('/api/add_tuning/:libelle',(req, res) => {
+    let libelle = req.params.libelle;
+    db.run(`INSERT INTO tunings (LIBELLE) VALUES (?)`, [libelle], function (err) {
+        console.log(libelle)
+       res.json({message: 'success'})
+   })
+});
+
+//UPDATE one tuning 
+app.use('/api/update_tuning/:id/:libelle',(req, res) => {
+    let id = req.params.id;
+    let libelle = req.params.libelle;
+    db.run(`UPDATE tunings SET libelle = ? WHERE id = ?`, [libelle, id], function (err) {
+        console.log(libelle)
+       res.json({message: 'success'})
+   })
+});
+
+//DELETE a tuning (id)
+app.use('/api/delete_tuning/:id',(req, res) => {
+    let id = req.params.id;
+    db.run(`DELETE FROM tunings WHERE ID = ${id}`, function (err) {
+        console.log(id)
+       res.json({message: 'success'})
+   })
+});
+
+module.exports = app;
